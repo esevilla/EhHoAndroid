9dc710196d38906e8a59c436911a27b00bc8dcf1
Estefania Sevilla <esevilla91@gmail.com>
2019-12-17 00:39:15 +0100

    dia 1 completado

--- app/src/main/java/io/esevilla/eh_ho/TopicsActivity.kt
+++ app/src/main/java/io/esevilla/eh_ho/TopicsActivity.kt
@@ -1,12 +1,12 @@
 package io.esevilla.eh_ho
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class TopicsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_topics)
     }
 }
